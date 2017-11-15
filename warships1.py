4ecb9fea1be44a27f89b5fa3c42d86f07783c382
Sunshine-Bucket <33370488+Sunshine-Bucket@users.noreply.github.com>
2017-11-15 22:01:42 +0000

    Add files via upload

--- warships1.py
+++ warships1.py
@@ -8,6 +8,7 @@ def ranpc():
 def rantt():
     return random.randint(1,10000)
 
+    
 class Battleship:
     
     def __init__(self,name):
@@ -17,12 +18,30 @@ class Battleship:
         self.armour = 50
         self.name = name
     
-    def shoot_at(self,target):
-        if self.firecontrol > ranpc():
-            print(target.name,"hit!")
+    def engage(self):
+        input_string = "Select target\n"
+        options = ships[:]
+        options.remove(self)
+        if len(options) == 1:
+            target=options[0]
+            print(target.name," selected as target")
+        else:
+           for i in range(len(options)):
+               input_string = input_string +"\t" + str(i) + ". " + options[i].name + "\n"
+           
+           select = input(input_string)
+           target = ships[int(select)]    
+        #print("check point")
+        distance = int(input("Enter range (in km):"))
+
+        self.shoot_at(target,distance)
+    
+    def shoot_at(self,target,distance):
+        if (self.firecontrol - distance) > ranpc():
+            print("Hit!\n\n")
             target.is_hit(self.firepower)
         else:
-            print(self.name,"missed")
+            print("Missed\n\n")
         pass        
     
     def is_hit(self,firepower):
@@ -40,6 +59,7 @@ class Battleship:
     def is_afloat(self):
         return not self.is_sunk()
         
+
 #print(ranpc())
 #print(random.randint(1,100))
 
@@ -47,23 +67,32 @@ w1 = Battleship("BISMARK")
 w2 = Battleship("HOOD")
 
 ships = [w1,w2]
-
+active_ship_actions = ["Fire","Abandon ship","Retreat"]
 
 #print("w2 is afloat is",w2.is_afloat())
 
 
 while w2.is_afloat() and w1.is_afloat():
-    action = int(input("""
-Select action:
-  1. HOOD shoots at BISMARK
-  2. BISMARK shoots at HOOD
-  """))
-  
-    if action == 2:
-        w1.shoot_at(w2)
-    else:
-        w2.shoot_at(w1)
-    #print("w2 is afloat is",w2.is_afloat())
+    input_string_1 = "Select ship\n"
+    for i in range(len(ships)):
+        input_string_1 = input_string_1 +"\t" + str(i) + ". " + ships[i].name + "\n"
+    
+    select_1 = input(input_string_1)
+    active_ship = ships[int(select_1)]
+    
+    
+    #print(active_ship.name)
+    input_string_2 = "Select action\n"
+    for i in range(len(active_ship_actions)):
+        input_string_2 = input_string_2 +"\t" + str(i) + ". " + active_ship_actions[i] + "\n"
+    
+    select_2 = input(input_string_2)
+    active_ship_action = active_ship_actions[int(select_2)]
+    
+    
+    if active_ship_action == "Fire":
+        active_ship.engage()
+
 
 
 #print("w1 health is ",w1.health,"w2 health is",w2.health)
